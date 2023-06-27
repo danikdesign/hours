803501fbae8038c331728c853e0dd78d7b2852f0
Daniil <100140433+danikdesign@users.noreply.github.com>
2023-06-27 22:41:22 +0300

    Added pages title functional, navbar, errors and flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  def main_menu 
+    render partial: 'shared/menu'
+  end
+  
+  # def nav_tab(title, url, options = {})
+  #   current_page = options.delete :current_page
+
+  #   css_class = current_page == title ? 'nav_link active' : 'nav_link'
+
+  #   options[:class] = if options[:class]
+  #                       "#{options[:class]} #{css_class}"
+  #                     else
+  #                       css_class
+  #                     end
+
+  #   link_to title, url, options
+  # end
+
+  # def currently_at(current_page = '')
+  #   render partial: 'shared/menu', locals: { current_page: }
+  # end
+
+  def full_title(page_title = '')
+    base_title = 'HOURS'
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
+
+  def prepend_flash
+    turbo_stream.prepend 'flashes', partial: 'shared/flashes'
+  end
 end
